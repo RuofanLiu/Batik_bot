0e118286e15e966b7412db77dc20101e428f40c1
Allen Liu <RuofanLiu@users.noreply.github.com>
2018-10-23 23:15:14 -0400

    now successfully finds the center
    now successfully finds the center (Still needs to be tested) by counting time and the number of steps

--- Batik_bot_final/Batik_bot_final.ino
+++ Batik_bot_final/Batik_bot_final.ino
@@ -4,6 +4,8 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <avr/pgmspace.h>                     // Store data in flash (program) memory instead of SRAM
+
+#define runningSpeed 200
 /* This program was created by ScottC on 8/5/2012 to receive serial
   signals from a computer to turn on/off 1-9 LEDs */
 
@@ -11,7 +13,7 @@
 int isFirstValue = 0;
 int maxScale; //this variable is used to determine the scale of the painting on the turning table.
 String val;
-int center = 0; //this variable stoers the number of steps needed for the syringe to reach the center of the turning table
+unsigned long center = 0; //this variable stoers the number of steps needed for the syringe to reach the center of the turning table
 //----------------------other variables--------------------//
 
 
@@ -99,36 +101,69 @@ int readTemp() {
 }
 
 /**
- * This function initialize the position of the lead screw and determines the center of the turning table
- * (this worked, but kinda buggy interms of the polar of the magnet issue (reset))
- */
-void initializeScrew(){
+   This function initialize the position of the lead screw and determines the center of the turning table
+   (this worked, but kinda buggy interms of the polar of the magnet issue (reset))
+*/
+void initializeScrew() {
+  /*
+     Use timer to help calculating the center
+  */
+  unsigned long startTime = 0;
+  unsigned long endTime = 0;
   int steps = 0;
   int reachedRightmost = 0;
   int reachedLeftmost = 0;
+  unsigned long duration = 0; //in seconds
+
   pinMode(hallLeadScrewRight, INPUT); //digit port 3
   pinMode(hallLeadScrewLeft, INPUT);  //digit port 4
-  while(reachedRightmost == 0 && hallStateLSright == LOW){
+  while (reachedRightmost == 0 && hallStateLSright == LOW) {
     hallStateLSright = digitalRead(hallLeadScrewRight);
     Rstepper.setSpeed(200);
     Rstepper.runSpeed();
   }
-  while(reachedRightmost == 0 && hallStateLSright == HIGH){
+  while (reachedRightmost == 0 && hallStateLSright == HIGH) {
     hallStateLSright = digitalRead(hallLeadScrewRight);
     Rstepper.setSpeed(200);
     Rstepper.runSpeed();
   }
   reachedRightmost = 1;
   delay(2000);
-  
-  while(reachedRightmost == 1 && hallStateLSleft == LOW){
+
+  if (hallStateLSright == HIGH) {
+    startTime = millis();
+  }
+  while (reachedRightmost == 1 && hallStateLSleft == LOW) {
     hallStateLSleft = digitalRead(hallLeadScrewLeft);
     Rstepper.setSpeed(-200);
     Rstepper.runSpeed();
   }
 
+  if (hallStateLSleft == HIGH) {
+    endTime = millis();
+  }
+  delay(5000);
+  /*Calculate the number of steps needed*/
+  //this doesnt work
+  duration = (endTime - startTime) / 1000;
+  Serial.print("Start time: ");
+  Serial.println(startTime);
+  Serial.print("End time: ");
+  Serial.println(endTime);
+  Serial.print("duration ");
+  Serial.println(duration);
+  center = (200 * duration) / 2;
+  //finally move the syringe to the center of the turning table, begin to draw
+
+  Rstepper.setCurrentPosition(0);
+  Rstepper.moveTo(center);
+  Serial.print("Steps to center: ");
+  Serial.println(center);
+  while (Rstepper.currentPosition() != Rstepper.targetPosition()) {
+    Rstepper.setSpeed(200);
+    Rstepper.runSpeedToPosition();
+  }
   reachedLeftmost = 1;
-  //TO DO Oct 23: Calculate the step ran and determine the midpoint
 }
 
 /**
@@ -141,16 +176,16 @@ void initializeTable() {
      If it is not at the initial position, continue turning
   */
   pinMode(hallTurningTable, INPUT);
-  
+
   while (hallStateTT == LOW) {
     hallStateTT = digitalRead(hallTurningTable);
     Astepper.setSpeed(100);
     Astepper.run();
   }
   /*
-   * since the US1881 hall sensor is blocked, move one step back to re-set its value to low
-   */
-  while(hallStateTT == HIGH) {
+     since the US1881 hall sensor is blocked, move one step back to re-set its value to low
+  */
+  while (hallStateTT == HIGH) {
     hallStateTT = digitalRead(2);
     Astepper.move(1);
     Astepper.run();
@@ -188,11 +223,14 @@ void setup() {
   val = Serial.readStringUntil('\n');
   delay(100); //this is necessary, as the arduino CPU is too slow to read data
   Serial.println("ACK\n");
+
   //------------------for stepper motor---------------------//
   AFMS.begin(1600);  // set the frequencey to 1.6k Hz
   AFMS_level2.begin(1600);
 
+  Rstepper.setMaxSpeed(1000);
   initializeTable();
+  delay(2000);
   initializeScrew();
   //myHeater->setSpeed(50);
   //------------------for stepper motor---------------------//
@@ -207,19 +245,19 @@ void loop() {
      The basic idea is, since the Arduino Uno memory is too small to store all the data pointes, Processing is used to send one set of data at a time
      When a set of data is received, send ACK to Processing so that the next data will be sent
   */
-    if (Serial.available() > 0) { // If data is available to read,
-      radii = Serial.readStringUntil(',').toDouble();
-      angle = Serial.readStringUntil('\n').toInt();
-  
-      Serial.print("radii: ");
-      Serial.println(radii);  //THIS LINE IS FOR TEST ONLY
-      Serial.print("angle: ");
-      Serial.println(angle);
-      Serial.print("maxScale: ");
-      Serial.println(maxScale);
-      delay(100);   //this is necessary FOR NOW
-      Serial.println("ACK\n");
-    }
+  if (Serial.available() > 0) { // If data is available to read,
+    radii = Serial.readStringUntil(',').toDouble();
+    angle = Serial.readStringUntil('\n').toInt();
+
+    Serial.print("radii: ");
+    Serial.println(radii);  //THIS LINE IS FOR TEST ONLY
+    Serial.print("angle: ");
+    Serial.println(angle);
+    Serial.print("maxScale: ");
+    Serial.println(maxScale);
+    delay(100);   //this is necessary FOR NOW
+    Serial.println("ACK\n");
+  }
 
 
   //Rstepper.run();  //this works, first accelerates and decellerates
